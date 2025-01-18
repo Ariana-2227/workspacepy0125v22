cd8e5a7f2ab69a2c5f64adb80cd60c1556f33b2e
Ariana-2227 <vanessa.asto.221203@gmail.com>
2025-01-18 22:15:56 +0000

    tarea1

---
+++ PRACTICA-PHYTON-DATUX.py
@@ -0,0 +1,26 @@
+#Pregunta N°04
+print("Hola mundo")
+
+#Pregunta N°05
+nombre= input("Escribe tu nombre : ")
+print(f"Hola , {nombre}")
+
+#Pregunta N°06
+edad= int(input("Escribe tu edad : "))
+if edad >= 18:
+    print ("Eres mayor de edad")
+else:
+    print ("No eres mayor de edad")
+
+#Pregunta N°07 
+numero=int(input("Ingresa un número entero, por favor: "))
+if numero % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
+
+#Pregunta N°08
+numero=int(input("Ingresa un número entero, por favor: "))
+suma= (numero*(numero + 1)) // 2 
+print(f"La suma de números enteros de 1 hasta {numero} es: {suma}")
+
